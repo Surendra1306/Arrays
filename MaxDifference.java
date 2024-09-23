7347ba7f663d6f90485771a46cc3ca2d7ef2c649
Surendra <118037065+Surendra1306@users.noreply.github.com>
2024-09-23 22:54:10 +0530

    reduced time complexity

--- MaxDifference.java
+++ MaxDifference.java
@@ -3,13 +3,24 @@ package Dsa_Arrays;
 class Solution {
     public static int maximumDifference(int[] nums) {
         int max = -1;
-        for(int i=0; i<nums.length-1; i++){
-            for(int j=i+1; j<nums.length; j++){
-                if(nums[i] < nums[j]){
-                    int temp = nums[j] - nums[i];
-                    max = Math.max(temp, max);
-                }
+//        for(int i=0; i<nums.length-1; i++){
+//            for(int j=i+1; j<nums.length; j++){
+//                if(nums[i] < nums[j]){
+//                    int temp = nums[j] - nums[i];
+//                    max = Math.max(temp, max);
+//                }
+//            }
+//        }
+//        return max;
+        
+//       TC = O(n) 
+        int min = nums[0];
+        for(int i=1; i<nums.length; i++){
+            int val = nums[i];
+            if(min < val){
+                max = Math.max(max, val-min);
             }
+            min = Math.min(min, val);
         }
         return max;
     }
